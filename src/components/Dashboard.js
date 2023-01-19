0c5f14c071ea2a117e1b1d14c3ee017c33601f35
Thien Trieu <thien.trieu@telus.com>
2023-01-19 10:56:59 -0800

    add panel component and mapped over data.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,46 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
 import classnames from "classnames";
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+
 
 export default class Dashboard extends Component {
   state = {
     loading: false
   };
 
+
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    const panels = data.map(panel => {
+      return <Panel key={panel.id} id={panel.id} label={panel.label} value={panel.value}/>
+    })
 
     if (this.state.loading) {
       return <Loading />;
