93849af9e2e1ea5f41fa8376c131033568eaf526
Thien Trieu <thien.trieu@telus.com>
2023-01-19 10:42:27 -0800

    First commit, npm install and start server. Add Loading component and state to Dashboard.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
-
+import Loading from "./Loading";
 import classnames from "classnames";
 
-class Dashboard extends Component {
+export default class Dashboard extends Component {
+  state = {
+    loading: false
+  };
+
   render() {
     const dashboardClasses = classnames("dashboard");
+
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
 }
-
-export default Dashboard;
